d0e1d8485a37a23cb47c6f880a36ef20be896858
farseen n k <farseennk2@gmail.com>
2025-09-13 20:12:12 +0530

    chore

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (window.scrollY > 50) {
       //change header srx to logo-2
       headerImage.src = "assets/SKC-LOGO-2.png";
-      header.classList.add("shadow-md");
-      header.classList.remove("");
+      header.classList.add("text-black");
+      header.classList.remove("text-white,shadow-md");
     } else {
       headerImage.src = "assets/SKC-LOGO-3.png";
-      header.classList.remove("shadow-md");
+      header.classList.remove("text-black");
+      header.classList.add("shadow-md,text-white");
     }
   });
 
